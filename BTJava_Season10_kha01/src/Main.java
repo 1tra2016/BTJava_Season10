6e3ba3eaeac6d754afaed5c81e460e855d1c2185
Chè <119411856+1tra2016@users.noreply.github.com>
2025-12-24 16:43:28 +0700

    Add files via upload

---
+++ BTJava_Season10_kha01/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog("Buddy");
+        d.makeSound();
+    }
+}
+
+
+
