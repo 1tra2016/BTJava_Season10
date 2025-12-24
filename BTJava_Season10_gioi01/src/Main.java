a2fd59630a05e7d12ddc34aee28756714f45c826
Chè <119411856+1tra2016@users.noreply.github.com>
2025-12-24 16:59:14 +0700

    Update Main.java

--- BTJava_Season10_gioi01/src/Main.java
+++ BTJava_Season10_gioi01/src/Main.java
@@ -3,12 +3,12 @@ public class Main {
         Computer c = new Computer();
 
         double p1 = c.calculatePrice(1000);
-        System.out.println("Giá phải trả: " + p1);
+        System.out.println("Giá gốc phải trả: " + p1);
 
         double p2 = c.calculatePrice(1000, 0.1);
-        System.out.println("Giá phải trả: " + p2);
+        System.out.println("Giá phải trả sau thuế: " + p2);
 
         double p3 = c.calculatePrice(1000, 0.1, 50);
-        System.out.println("Giá phải trả: " + p3);
+        System.out.println("Giá phải trả sau thuế có giảm giá: " + p3);
     }
 }
